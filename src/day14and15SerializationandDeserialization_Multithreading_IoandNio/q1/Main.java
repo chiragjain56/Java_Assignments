2ce307400e4ec07fb525e4f3d39c51d04409e5dd
chiragjain5june <chiragjain5june@gmail.com>
2022-03-28 07:14:06 +0530

    q1.

---
+++ src/day14and15SerializationandDeserialization_Multithreading_IoandNio/q1/Main.java
@@ -0,0 +1,20 @@
+package day14and15SerializationandDeserialization_Multithreading_IoandNio.q1;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    Student student = new Student(56,"Chirag","Udaipur","chirag@56","chirag123");
+    FileOutputStream fileOutputStream = new FileOutputStream("student.txt");
+    ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+    objectOutputStream.writeObject(student);
+        System.out.println("Done");
+
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("student.txt"));
+        Student student1 = (Student)objectInputStream.readObject();
+        System.out.println(student1.toString());
+    }
+}
